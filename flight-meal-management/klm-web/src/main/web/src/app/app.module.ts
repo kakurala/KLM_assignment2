75a138f545ff7c4232d42fbb6a8252b0134a8e43
madhusudhan kakurala <kakuralamadhu@gmail.com>
2019-07-10 15:33:29 +0530

    Dependency code missing issue fixed.

--- flight-meal-management/klm-web/src/main/web/src/app/app.module.ts
+++ flight-meal-management/klm-web/src/main/web/src/app/app.module.ts
@@ -7,7 +7,6 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TypeaheadModule } from 'ngx-bootstrap/typeahead';
 import { NgxLoadingModule } from 'ngx-loading';
-import { HelloComponent } from './test/hello/hello.component';
 import { AddFlightComponent } from './add-flight/add-flight.component';
 import { AddMealsComponent } from './add-meals/add-meals.component';
 import { DeleteFlightComponent } from './delete-flight/delete-flight.component';
@@ -16,7 +15,6 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HelloComponent,
     AddFlightComponent,
     AddMealsComponent,
     DeleteFlightComponent,
